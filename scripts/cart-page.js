00420872ad5b9b9ba61d36dc8cc7e60dd20f3c91
Minna <minnalnh@gmail.com>
2026-03-12 14:58:18 +0100

    continued on shopping cart page

--- scripts/cart-page.js
+++ scripts/cart-page.js
@@ -4,13 +4,13 @@ displayOrder();
 
 function displayOrder() {
 
-    let fullOrder = JSON.parse(localStorage.getItem("orderedItems")) || [];
+    let fullOrder = JSON.parse(localStorage.getItem('orderedItems')) || [];
     const orderedItems = JSON.parse(localStorage.getItem('orderedItems'));
 
-    console.log(fullOrder);
-
     const menuRef = getElement('.menu');
+    const totalRef = getElement('.total');
     let orderTemplate = '';
+    let totalSumTemplate = '';
 
     for(let i = 0; i < orderedItems.length; i++) {
         orderTemplate = `
@@ -18,11 +18,35 @@ function displayOrder() {
                 <p class="order__card">
                     <span class="order__name">${orderedItems[i].name}</span>
                     <span class="order__price">${orderedItems[i].price} kr</span>
-                    <span class="order__quantity">${orderedItems[i].quantity} st</span>
                 </p>
+                <p class="order__quantity">${orderedItems[i].quantity} stycken</p>
             </article>
         `;
         menuRef.innerHTML += orderTemplate;
 
     }
+
+    const totalSum = calcTotalPrice(fullOrder);
+
+        totalSumTemplate = `
+            <p class="total__text">Totalt</p>
+            <p class="total__price">${totalSum} sek</p>
+        `;
+        totalRef.innerHTML += totalSumTemplate;
+}
+
+function calcTotalPrice(fullOrder) {
+    const priceArr = [];
+    let sum = 0;
+    let priceTimesQuantity = 0;
+    
+    for(let order of fullOrder) {
+        priceTimesQuantity = order.price * order.quantity;
+        priceArr.push(priceTimesQuantity);
+    }
+    
+    for(let i = 0; i < priceArr.length; i++) {
+        sum += priceArr[i];    
+    }
+    return sum;
 }
