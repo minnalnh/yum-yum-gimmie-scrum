dba128a24a8d8564fd79b6d2fd7ad07d63935c47
Ler98 <lisa.eriksson98@hotmail.com>
2026-03-17 10:40:36 +0100

    bytte knappen mot kryss och tog bort varukorgen

--- scripts/orderhistory.js
+++ scripts/orderhistory.js
@@ -1,12 +1,22 @@
 import { getElement } from "./utils/domUtils.js";
 
-const showOrder = getElement(".btn"); 
+// const showOrder = getElement(".btn"); 
 
-showOrder.addEventListener("click", (e) => {
-    const ikonM = e.target.closest("showOrder");
+// showOrder.addEventListener("click", (e) => {
+//     const ikonM = e.target.closest("showOrder");
+    
+//         window.location.href ="profile.html"
+//         console.log("klick");
+        
+//     return;
+// })
+
+const iconClose = getElement(".header__icon--close"); 
+
+iconClose.addEventListener("click", (e) => {
+    const ikonC = e.target.closest("iconClose");
     
         window.location.href ="profile.html"
         console.log("klick");
         
-    return;
 })
