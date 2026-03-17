d51b6b8b3fe43142d0fc3f8cd3ccb96a4c84d33a
Ler98 <lisa.eriksson98@hotmail.com>
2026-03-17 21:27:27 +0100

    orderhistoriken syns på sidan

--- scripts/orderhistory.js
+++ scripts/orderhistory.js
@@ -1,5 +1,8 @@
 import { getElement } from "./utils/domUtils.js";
 
+
+
+//gjorde en knapp först, men kryss ser nog bättre ut, för varukorgen behövs inte här.
 // const showOrder = getElement(".btn"); 
 
 // showOrder.addEventListener("click", (e) => {
@@ -20,3 +23,111 @@ iconClose.addEventListener("click", (e) => {
         console.log("klick");
         
 })
+
+
+const orders = JSON.parse(localStorage.getItem("orderInformations")) || [];
+
+orders.forEach(order => {
+    const orderSection = document.createElement("section");
+    const orderProdukt = document.createElement("section");
+
+    orderSection.classList.add ("order") ;
+    orderProdukt.classList.add ("order__innehåll");
+
+    const textDate = document.createElement("p")
+    const textOrderNummer = document.createElement("p")
+
+    textDate.innerText = `Datum:  ${order.date}`;
+    textOrderNummer.classList.add ("order__description");
+    textOrderNummer.innerText = `Ordernummer:  ${order.orderNumbers}`;
+    textDate.classList.add ("order__date");
+
+    orderSection.appendChild(textDate);
+    orderProdukt.appendChild(textOrderNummer);
+
+    order.food.forEach(food => {
+    const textOrder = document.createElement("p")
+    const textPrice = document.createElement("p")
+    const textAntal = document.createElement("p")
+    
+
+    textOrder.classList.add ("order__produktNamn");
+    textPrice.classList.add ("order__pris");
+    textAntal.classList.add ("order__antal");
+    
+    
+    textOrder.innerText = food.name;
+    textPrice.innerText = `Pris:  ${food.price}`;
+    textAntal.innerText = `Antal:  ${food.quantity}`;
+    
+
+    
+    
+    orderProdukt.appendChild(textOrder);
+    orderProdukt.appendChild(textPrice);
+
+    
+    orderSection.appendChild(textAntal)
+    
+    orderSection.appendChild(orderProdukt);
+
+    
+    })
+    document.querySelector(".orderContainer").appendChild(orderSection);
+})
+
+
+
+
+
+///gammla namn på klasser mm
+// const orders = JSON.parse(localStorage.getItem("orderInformations")) || [];
+
+// orders.forEach(order => {
+//     const orderSection = document.createElement("section");
+//     const orderProdukt = document.createElement("section");
+
+//     orderSection.classList.add ("card") ;
+//     orderProdukt.classList.add ("card__produkt");
+
+//     const textDate = document.createElement("p")
+//     const textOrderNummer = document.createElement("p")
+
+//     textDate.innerText = `Datum:  ${order.date}`;
+//     textOrderNummer.classList.add ("card__description");
+//     textOrderNummer.innerText = `Ordernummer:  ${order.orderNumbers}`;
+//     textDate.classList.add ("card__date");
+
+//     orderSection.appendChild(textDate);
+//     orderProdukt.appendChild(textOrderNummer);
+
+//     order.food.forEach(food => {
+//     const textOrder = document.createElement("p")
+//     const textPrice = document.createElement("p")
+//     const textAntal = document.createElement("p")
+    
+
+//     textOrder.classList.add ("card__info");
+//     textPrice.classList.add ("card__sek");
+//     textAntal.classList.add ("card__antal");
+    
+    
+//     textOrder.innerText = `Order:  ${food.name}`;
+//     textPrice.innerText = `Pris:  ${food.price}`;
+//     textAntal.innerText = `Antal:  ${food.quantity}`;
+    
+
+    
+    
+//     orderProdukt.appendChild(textOrder);
+//     orderProdukt.appendChild(textPrice);
+
+    
+//     orderSection.appendChild(textAntal)
+    
+//     orderSection.appendChild(orderProdukt);
+
+    
+//     })
+//     document.querySelector(".orderContainer").appendChild(orderSection);
+// })
