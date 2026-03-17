3e2b815b7bf40387061cf4f2a4d54e919bfb6bbe
Minna <minnalnh@gmail.com>
2026-03-17 12:56:02 +0100

    added products in cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -37,4 +37,31 @@ function cartSetup() {
 
 function displayCartItems(cartItemsRef) {
     const fullOrder = getOrder();
+
+    for(let i = 0; i < fullOrder.length; i++) {
+        const orderTemplate = `
+            <article class="menu__card" data-id="${fullOrder[i].id}"> 
+                <p class="menu__cardHeader">
+                    <span>${fullOrder[i].name}</span> 
+                    <span class="element-order">${fullOrder[i].price} kr</span>
+                </p>
+                <p class="order__quantity">${fullOrder[i].quantity} stycken</p>
+                <section class="menu__btn-section"> 
+                    <button aria-label="Ta bort produkt från beställning" class="menu__cardDelete">
+                        -
+                    </button> 
+                    <p class="menu__cardQuantity">
+                        ${fullOrder[i].quantity}
+                    </p> 
+                    <button aria-label="Lägg till produkt i beställning" class="menu__cardAdd">
+                        +
+                    </button> 
+                    <button aria-label="Lägg till i varukorg" class="menu__card-update-button btn--red"> 
+                        Uppdatera varukorg 
+                    </button>
+                </section>
+            </article>
+        `;
+        cartItemsRef.innerHTML += orderTemplate;
+    }
 }
